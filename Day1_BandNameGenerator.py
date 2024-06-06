40296984c2da7ffd66f0b095018a8eab3a42d091
HarsheetT <tharsheet2912@gmail.com>
2024-06-06 16:42:40 +0530

    Day1_BandNameGenerator

---
+++ Day1_BandNameGenerator.py
@@ -0,0 +1,11 @@
+#Greetings
+print("Welcome! To the Band Name Generator")
+
+#Ask the user for their city name
+city_name = input("Name of your city: \n")
+
+# Ask the user for the name of a pet
+pet_name = input("Name of your pet: \n")
+
+# Combine the city_name and pet_name and show their band name
+print("Band Name: " + city_name + " " + pet_name)
